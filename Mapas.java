e513af53121ddd49e220a990d2753a818388dc51
Angelo1622 <jangelocr1@gmail.com>
2024-11-05 09:21:36 -0500

    listo

--- Mapas.java
+++ Mapas.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Mapas {
      int[][] mapa1 = {
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -23,4 +24,8 @@ public class Mapas {
             {1,2,2,2,1,2,1,2,1,2,2,2,2,2,1,1,2,2,2,2,2,1,2,1,2,1,2,2,2,1},
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
         };
+    
+    ArrayList <int[]> mapas = new ArrayList<>();
+    mapas.add(mapa1);
+    mapas.add(mapa2);
 }
